6cbbe53f9e44e7359a17e726d02b95ee2d12be8c
Nicholas Deng <nicholas.deng@worldrapidfinance.com>
2021-05-10 20:21:04 -0400

    db template implemented, homepage image link fixed

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,36 +1,34 @@
 
-var fetch = require('node-fetch');
+// var fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/form', function(req, res, next) {
-  res.render("ratings_form");
-});
+const {fetchProducts} = require("../firebase-service")
+
+/* GET course rating. */
+router.get('/', async function(req, res, next) {
+  try {
+      var products = await fetchProducts()
+      res.render("ratings_form", {"ratings_form": products})
+  } catch (error) {
+      req.flash("danger", "OOPS, failed to fetch products.")
+      res.redirect("/")
+  }
+})
 
 router.post('/dashboard', function(req, res, next) {
-    console.log("FORM DATA", req.body)
-    var symbol = req.body.symbol || "OOPS"
-    console.log("STOCK SYMBOL", symbol)
-  
-   // var requestUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}` // using string interpolation here, but you could alternatively do concatenation with + operators
-   // console.log("REQUEST URL", requestUrl)
-  
-    fetch(requestUrl)
-      .then(function(response) {
-          return response.json()
-      })
-      .then(function(data){
-          console.log("RATINGS DATA SUCCESS", Object.keys(data))
-          //var latestClose = Object.values(data["Time Series (Daily)"])[0]["5. adjusted close"]
-          req.flash("success", "Ratings Data Request Success!")
-          res.render("ratings_dashboard", {symbol: symbol, data: JSON.stringify(data), latestClose: latestClose});
-        })
-      .catch(function(err){
-        console.log("RATINGS DATA ERROR:", err)
-        req.flash("danger", "OOPS, Please check your inputs and try again.")
-        res.redirect("/ratings/form")
-      })
-  });
+  console.log("FORM DATA", req.body)
+  var productId = req.body.productId
+  var productName = req.body.productName
+  var productPrice = req.body.productPrice
+  // todo: maybe update the form / flow to ask the user for this info as well...
+  //var userEmail = "customer@example.com"
+  //var quantity = 1
+  //var totalPrice = productPrice * quantity
+  console.log("TODO: ORDER PRODUCT", productId, productName, productPrice)
+
+  req.flash("warning", "Order sent successfully (TODO)!")
+  res.redirect("/ratings_form")
+})
 
 module.exports = router;
