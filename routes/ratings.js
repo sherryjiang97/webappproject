132e7e1fa554492e91c8ef104f6eb70c3fae12a3
Nicholas Deng <nicholas.deng@worldrapidfinance.com>
2021-05-11 21:49:25 -0400

    average score display implemented

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,5 +1,4 @@
 
-// var fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
 
@@ -22,11 +21,39 @@ router.post('/dashboard', async function(req, res, next) {
     req.flash("danger", "OOPS, failed to fetch course.")
     res.redirect("/")
   }
+  var difficulty = []
+  var assessments = []
+  var groupWork = []
+  reviews.forEach(function(review) {
+    difficulty.push(review.difficulty)
+    assessments.push(review.assessments)
+    groupWork.push(review.groupWork)
+  })
 
+  var difficultyTotal = 0;
 
-  res.render("ratings_dashboard", {"reviews": reviews})
+  var groupWorkTotal = 0;
+  for(var i = 0; i < difficulty.length; i++) {
+      difficultyTotal += difficulty[i];
+  }
+  var avgDifficulty = difficultyTotal / difficulty.length;
 
-  req.flash("warning", "Order sent successfully (TODO)!")
+  var assessmentsTotal = 0;
+  for(var i = 0; i < assessments.length; i++) {
+    assessmentsTotal += assessments[i];
+  }
+  var avgAssessments = assessmentsTotal / assessments.length;
+
+  for(var i = 0; i < groupWork.length; i++) {
+    groupWorkTotal += groupWork[i];
+  }
+  var avgGroupWork = groupWorkTotal / groupWork.length;
+
+  var avgScores = [avgDifficulty, avgAssessments, avgGroupWork];
+
+  res.render("ratings_dashboard", {"reviews": reviews, "averages": avgScores})
+
+  req.flash("warning", "Ratings retrieved successfully!")
   //res.redirect("/ratings_form")
 })
 
--- routes/submit.js
+++ routes/submit.js
@@ -15,22 +15,21 @@ router.post('/success', async function(req, res, next) {
   var date = new Date();
   var rate = {
     "course_name": req.body.course_name,
-    "assessments": req.body.assessments,
-    "difficulty": req.body.difficulty,
-    "groupWork": req.body.groupWork,
+    "assessments": parseInt(req.body.assessments),
+    "difficulty": parseInt(req.body.difficulty),
+    "groupWork": parseInt(req.body.groupWork),
+    "comments": req.body.comments,
     "submitTime": date
   }
 
   try {
     var submit = await submitRatings(rate);
   } catch {
-    req.flash("danger", "OOPS, failed to fetch course.")
+    req.flash("danger", "OOPS, failed to submit ratings.")
     res.redirect("/")
   }
-
-
+  
   res.render("submit_success")
-
 
   req.flash("warning", "Ratings sent successfully!")
   res.redirect("/submit")
